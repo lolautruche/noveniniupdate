30f7d7058095dcbf50a714ab41a00233fe0fa494
Jérôme Vieilledent <lolautruche@gmail.com>
2010-05-02 00:12:52 +0000

    Removed useless regional settings in site.ini

--- settings/site.ini.append.php
+++ settings/site.ini.append.php
@@ -1,13 +1,6 @@
 <?php /* #?ini charset="utf-8"?
 
 [RegionalSettings]
-Locale=fre-FR
-ContentObjectLocale=fre-FR
-TextTranslation=enabled
-TranslationCache=enabled
 TranslationExtensions[]=noveniniupdate
-
-[ContentSettings]
-TranslationList=fre-FR
 
 */ ?>

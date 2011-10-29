e9886265bbc2e3a1b6132a6c19f35f910cddb965
Alexandre SEBBANE <alex@sebbane.org>
2011-10-29 10:22:36 +0200

    Move the admin visualization/switch to the Setup tab

--- settings/menu.ini.append.php
+++ settings/menu.ini.append.php
@@ -1,27 +1,9 @@
 <?php /* #?ini charset="utf-8"?
 
-[NavigationPart]
-Part[noveniniupdatenavigationpart]=INI Config
-
-[TopAdminMenu]
-Tabs[]=noveniniupdate
-
-[Topmenu_noveniniupdate]
-NavigationPartIdentifier=noveniniupdatenavigationpart
-Name=INI Config
-Tooltip=Edit INI files depending on your environment (ie. prod, preprod...)
-URL[]
-URL[default]=noveniniupdate/view
-Enabled[]
-Enabled[default]=true
-Enabled[browse]=false
-Enabled[edit]=false
-Shown[]
-Shown[default]=true
-Shown[navigation]=true
-Shown[browse]=false
-
 # Tabs policy on admin interface in 4.3+
-PolicyList[]=noveniniupdate/configupdate
+[Leftmenu_setup]
+Links[noveniniupdate]=noveniniupdate/view
+LinkNames[noveniniupdate]=INI Config
+PolicyList_noveniniupdate[]=noveniniupdate/configupdate
 
 */ ?>
